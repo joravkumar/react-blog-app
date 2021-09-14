d9ef5d270097676e132f60935f6ecdd2942856e8
jorav <jorav@kochartech.com>
2021-09-14 17:56:48 +0530

    feat: menu active logic

--- src/Header.js
+++ src/Header.js
@@ -1,46 +1,93 @@
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useLocation } from 'react-router-dom'; 
 
 function Header() {
-  return <div>
-    <div>
-      <div className="container">
-        <div className="header">
-          <div className="header-nav">
-            <nav className="navbar navbar-default">
-              <div className="navbar-header">
-                <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                  <span className="sr-only">Toggle navigation</span>
-                  <span className="icon-bar"></span>
-                  <span className="icon-bar"></span>
-                  <span className="icon-bar"></span>
-                </button>
-                <a className="navbar-brand" href="index.html"><span>Q</span>uickly</a>
-              </div>
+  const [activeMenu, setActiveMenu] = useState("");
+  const location = useLocation();
 
-              <div className="collapse navbar-collapse nav-wil" id="bs-example-navbar-collapse-1">
-                <ul className="nav navbar-nav">
-                  <li className="hvr-bounce-to-bottom active"><Link to="/">Home</Link></li>
-                  <li className="hvr-bounce-to-bottom"> <Link to="/About">About</Link></li>
-                  <li className="hvr-bounce-to-bottom"> <Link to="/Portfolio">Portfolio</Link></li>
-                  <li className="hvr-bounce-to-bottom"><Link to="/Contact">Contact</Link></li>
-                </ul>
-                <div className="sign-in">
-                  <ul>
-                    <li><a href="login.html">Sign In </a>/</li>
-                    <li><a href="register.html">Register</a></li>
-                  </ul>
+  useEffect(() => {
+    console.log('Location changed', location.pathname);
+    setActiveMenu(location.pathname)
+  }, [location]);
+  return (
+    <div>
+      <div>
+        <div className="container">
+          <div className="header">
+            <div className="header-nav">
+              <nav className="navbar navbar-default">
+                <div className="navbar-header">
+                  <button
+                    type="button"
+                    className="navbar-toggle collapsed"
+                    data-toggle="collapse"
+                    data-target="#bs-example-navbar-collapse-1"
+                  >
+                    <span className="sr-only">Toggle navigation</span>
+                    <span className="icon-bar"></span>
+                    <span className="icon-bar"></span>
+                    <span className="icon-bar"></span>
+                  </button>
+                  <Link className="navbar-brand" to="/">
+                    <span>Q</span>uickly
+                  </Link>
                 </div>
-              </div>
-            </nav>
+
+                <div
+                  className="collapse navbar-collapse nav-wil"
+                  id="bs-example-navbar-collapse-1"
+                >
+                  <ul className="nav navbar-nav">
+                    <li
+                      className={`hvr-bounce-to-bottom ${
+                        activeMenu === "/" ? "active" : ""
+                      }`}
+                    >
+                      <Link to="/">Home</Link>
+                    </li>
+                    <li
+                      className={`hvr-bounce-to-bottom ${
+                        activeMenu === "/About" ? "active" : ""
+                      }`}
+                    >
+                      {" "}
+                      <Link to="/About">About</Link>
+                    </li>
+                    <li
+                      className={`hvr-bounce-to-bottom ${
+                        activeMenu === "/Portfolio" ? "active" : ""
+                      }`}
+                    >
+                      {" "}
+                      <Link to="/Portfolio">Portfolio</Link>
+                    </li>
+                    <li
+                      className={`hvr-bounce-to-bottom ${
+                        activeMenu === "/Contact" ? "active" : ""
+                      }`}
+                    >
+                      <Link to="/Contact">Contact</Link>
+                    </li>
+                  </ul>
+                  <div className="sign-in">
+                    <ul>
+                      <li>
+                        <a href="login.html">Sign In </a>/
+                      </li>
+                      <li>
+                        <a href="register.html">Register</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </nav>
+            </div>
           </div>
         </div>
-
       </div>
     </div>
-  </div>
-
-
-};
+  );
+}
 
 export default Header;
-
